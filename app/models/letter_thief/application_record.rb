9c443c05509a52f8507e7fc8576660670cf103f8
Alessandro Rodi <alessandro.rodi@renuo.ch>
2026-09-10 19:59:31 +0200

    Fix connects_to

--- app/models/letter_thief/application_record.rb
+++ app/models/letter_thief/application_record.rb
@@ -1,5 +1,7 @@
 module LetterThief
   class ApplicationRecord < ActiveRecord::Base
     self.abstract_class = true
+
+    connects_to(**LetterThief.connects_to) if LetterThief.connects_to
   end
 end
--- test/dummy/config/initializers/letter_thief.rb
+++ test/dummy/config/initializers/letter_thief.rb
@@ -1,3 +1,5 @@
 # Configure LetterThief to use a custom base controller for authentication
 # Uncomment the line below to test the base_controller_class configuration:
 # LetterThief.base_controller_class = "AdminController"
+
+LetterThief.connects_to = {database: {writing: :letter_thief}} if ENV["LETTER_THIEF_SEPARATE_DB"] == "true"
