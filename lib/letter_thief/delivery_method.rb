54548c32e42c3c7c3f8b67826b9d74187f666541
wivern-co-uk <echo@wivern.co.uk>
2026-09-10 20:05:44 +0200

    Fix email persistence when launchy is not installed (#72)

--- lib/letter_thief/delivery_method.rb
+++ lib/letter_thief/delivery_method.rb
@@ -6,8 +6,9 @@ module LetterThief
     end
 
     def deliver!(mail)
+      delivered_mail = Observer.delivered_email(mail)
       require "launchy"
-      ::Launchy.open(LetterThief::Engine.routes.url_helpers.email_message_url(Observer.delivered_email(mail), Rails.configuration.action_mailer.default_url_options))
+      ::Launchy.open(LetterThief::Engine.routes.url_helpers.email_message_url(delivered_mail, Rails.configuration.action_mailer.default_url_options))
     rescue LoadError
       puts "WARNING: LetterThief requires the 'launchy' gem to open the email in a web browser. Add it to your Gemfile."
     end
